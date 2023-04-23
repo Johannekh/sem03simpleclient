bd4ae177430fa7d0ede880576ee51d487e596e05
Johannekh <johanne.haakenstad@icloud.com>
2023-04-23 22:23:37 +0000

    pakker

--- tcp_client.go
+++ tcp_client.go
@@ -8,13 +8,15 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.4:3000")
+	conn, err := net.Dial("tcp", "172.17.0.5:3000")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
-
+kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+log.Println("Kryptert melding: ", string(kryptertMelding))
+_, err = conn.Write([]byte(string(kryptertMelding)))
  	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
 		log.Fatal(err)
@@ -27,8 +29,6 @@ func main() {
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
 
-
-kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-log.Println("Kryptert melding: ", string(kryptertMelding))
-_, err = conn.Write([]byte(string(kryptertMelding)))
+        dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+        log.Println("Dekrypter melding: ", string(dekryptertMelding))
 }
